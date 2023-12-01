b103d90013e48c235e6e6b6aa1ea3b74326987c8
Marjolaine BOUQUET <mbouquet77@protonmail.com>
2023-12-01 22:34:41 +0100

    Day1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -10,9 +10,9 @@ public class Program
 
         var calib = new Calibration();
         var res = calib.GetCalibrationSumPart1(lines);
-        Console.WriteLine(res);
+        Console.WriteLine($"star1={res}");
 
         var res2 = calib.GetCalibrationSumPart2(lines);
-        Console.WriteLine(res2);
+        Console.WriteLine($"star2={res2}");
     }
 }
